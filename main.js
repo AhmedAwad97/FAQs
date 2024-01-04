4f88edd8537876ddc951f521d87068dfa95fcf04
Ahmed Awad <awad55397@gmail.com>
2024-01-05 00:48:18 +0100

    edit js code

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ let collapsable = document.querySelectorAll(".question-line");
 collapsable.forEach((line) => {
   line.addEventListener("click", () => {
     line.parentNode.classList.toggle("hidden");
-    let icon = line.querySelector(".collapsable");
+    let icons = line.querySelectorAll(".collapsable");
 
     line.parentNode.classList.contains("hidden")
-      ? icon.setAttribute("src", "assets/images/icon-plus.svg")
-      : icon.setAttribute("src", "assets/images/icon-minus.svg");
+      ? icons.forEach((icon) =>
+          icon.setAttribute("src", "assets/images/icon-plus.svg")
+        )
+      : icons.forEach((icon) =>
+          icon.setAttribute("src", "assets/images/icon-minus.svg")
+        );
   });
 });
