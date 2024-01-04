19135470e3ac7a97a778a4a2fcb32e420c6a4e5e
Ahmed Awad <awad55397@gmail.com>
2024-01-05 00:31:43 +0100

    enhance the code

--- main.js
+++ main.js
@@ -6,16 +6,7 @@ collapsable.forEach((line) => {
     let icon = line.querySelector(".collapsable");
 
     line.parentNode.classList.contains("hidden")
-      ? icon.setAttribute("src", "./assets/images/icon-plus.svg")
-      : icon.setAttribute("src", "./assets/images/icon-minus.svg");
+      ? icon.setAttribute("src", "assets/images/icon-plus.svg")
+      : icon.setAttribute("src", "assets/images/icon-minus.svg");
   });
 });
-
-// collapsable.forEach((icon) => {
-//   icon.addEventListener("click", () => {
-//     icon.parentNode.parentNode.classList.toggle("hidden");
-//     icon.parentNode.parentNode.classList.contains("hidden")
-//       ? (icon.src = "./assets/images/icon-plus.svg")
-//       : (icon.src = "./assets/images/icon-minus.svg");
-//   });
-// });
