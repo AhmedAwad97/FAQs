3a06f15560fea62865808c267385162d4a55bde8
Ahmed Awad <awad55397@gmail.com>
2024-01-05 01:53:03 +0100

    trying to fix the collapsable icon

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let questionLines = document.querySelectorAll(".question-line");
+  let minus = document.querySelectorAll(".row.minus  .collapsable");
+  minus.forEach((col) =>
+    col.setAttribute("src", "assets/images/icon-minus.svg")
+  );
 
+  let plus = document.querySelectorAll(".row.hidden  .collapsable");
+  plus.forEach((col) => col.setAttribute("src", "assets/images/icon-plus.svg"));
+
+  let questionLines = document.querySelectorAll(".question-line");
   questionLines.forEach((line) => {
     line.addEventListener("click", () => {
       line.parentNode.classList.toggle("hidden");
